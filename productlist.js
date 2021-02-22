fb94c7f0828be1bd99964bc114fd0b00851ca3b9
Berglind Omarsdottir <berg0279@stud.kea.dk>
2021-02-22 17:09:58 +0100

    productlist-fetching

---
+++ productlist.js
@@ -0,0 +1,38 @@
+const url = "https://kea21-6a0c.restdb.io/rest/wine?max=35";
+
+//The API key
+const options = {
+    headers: {
+        "x-apikey": "60339bce5ad3610fb5bb64e6",
+    },
+};
+
+fetch(url, options)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        handleData(data);
+    })
+    function handleData(data){
+        data.forEach(product);
+    }
+    function product(wine){
+        console.log(wine);
+
+        //grab template
+        const template = document.querySelector("template").content;
+
+        //clone it
+        const clone = template.cloneNode(true);
+
+        //change something
+        clone.querySelector(".colour").textContent = wine.colour;
+        clone.querySelector(".name").textContent = wine.name;
+
+        //choose parent
+        const parent = document.querySelector("main");
+
+        //append
+        parent.appendChild(clone);
+    }
