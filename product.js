2c845405a1203fed7e6c44febf51cd1123858c69
Leidi-Liisa Laane <leid0016@stud.kea.dk>
2021-02-23 13:33:32 +0100

    js

---
+++ product.js
@@ -0,0 +1,28 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+const url = `https://kea21-6a0c.restdb.io/rest/wine/` + id;
+
+//API key
+//The API key
+const options = {
+  headers: {
+    "x-apikey": "60339bce5ad3610fb5bb64e6",
+  },
+};
+
+// fetching data
+fetch(url, options)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+
+function showProduct(wine) {
+  console.log(wine);
+
+  document.querySelector(".name").textContent = wine.name;
+  document.querySelector(".brand").textContent = wine.brand;
+  document.querySelector(
+    ".image"
+  ).src = `https://kea21-6a0c.restdb.io/media/${wine.bottle}`;
+  document.querySelector(".image").alt = wine.name;
+}
