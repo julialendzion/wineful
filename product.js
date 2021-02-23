abf3f61bec5f061a34c4b2d9ca28f51836a3cb99
Nanhe Kumar <info@nanhekumar.com>
2021-02-23 12:43:17 +0100

    javaproduct

---
+++ product.js
@@ -0,0 +1,25 @@
+const url = `https://kea21-6a0c.restdb.io/rest/wine?max=35`;
+
+//API key
+//The API key
+const options = {
+  headers: {
+    "x-apikey": "60339bce5ad3610fb5bb64e6",
+  },
+};
+
+// fetching data
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+
+function showProduct(wine) {
+  console.log(wine);
+
+  document.querySelector(".name").textContent = wine.name;
+  document.querySelector(".brand").textContent = wine.brand;
+  document.querySelector(
+    ".image"
+  ).src = `https://kea21-6a0c.restdb.io/media/${wine.bottle}`;
+  clone.querySelector(".image").alt = wine.name;
+}
